c1c73b604533b73130289a209598b176e7b75665
gpcsantos <gpcsantos@gmail.com>
2023-07-01 12:21:39 -0300

    reconfig Media e Statics on settings

--- canesgrill/settings.py
+++ canesgrill/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'canesgrill.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -161,15 +161,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_ROOT = BASE_DIR / 'static'
+# STATIC_URL = 'static/'
+# STATICFILES_DIRS = [
+#      BASE_DIR / 'templates/static/', 
+#     ]
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-     BASE_DIR / 'templates/static/', 
-    ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "templates/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 ## Media
-MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Messagem
 MESSAGE_TAGS = {
